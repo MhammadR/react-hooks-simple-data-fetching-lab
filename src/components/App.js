ffc1c78c238673867d61a1cea6aa2b395bd54de5
MhammadR <114111412+MhammadR@users.noreply.github.com>
2022-12-04 19:05:08 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
-// create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("https://dog.ceo/api/breeds/image/random");
+      const res = await data.json();
+      setUrl(res.message);
+    };
+    fetchData().catch((error) => console.log("Error:", error));
+  }, []);
+
+  return (
+    <div>
+      {url ? <img src={url} alt="A Random Dog" /> : <p>"Loading..." </p>}
+    </div>
+  );
+}
+
+export default App;
